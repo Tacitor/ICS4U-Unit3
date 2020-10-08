e3cae62c95cf41d4763b7c24ee5cd2df9de8e64d
lukra1175 <lukra1175@8T71C62.Student.UGDSB.ED>
2020-10-08 14:57:30 -0400

    Update GUI Exercises Project 2, add JTable.

--- GUI Exercises/GUIExercise2/src/guiexercise2/InvestCalc.java
+++ GUI Exercises/GUIExercise2/src/guiexercise2/InvestCalc.java
@@ -46,8 +46,8 @@ public class InvestCalc extends javax.swing.JFrame {
         yearsSpn = new javax.swing.JSpinner();
         princeTxt = new javax.swing.JTextField();
         calcBtn = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        outputTxtar = new javax.swing.JTextArea();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        outputTbl = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -82,10 +82,30 @@ public class InvestCalc extends javax.swing.JFrame {
             }
         });
 
-        outputTxtar.setEditable(false);
-        outputTxtar.setColumns(20);
-        outputTxtar.setRows(5);
-        jScrollPane1.setViewportView(outputTxtar);
+        outputTbl.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Year", "Beg Value", "Amt Interest", "End Value"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane2.setViewportView(outputTbl);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -106,8 +126,8 @@ public class InvestCalc extends javax.swing.JFrame {
                             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                 .addComponent(rateSpn, javax.swing.GroupLayout.DEFAULT_SIZE, 53, Short.MAX_VALUE)
                                 .addComponent(yearsSpn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 646, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 634, Short.MAX_VALUE)
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -115,6 +135,7 @@ public class InvestCalc extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(rateLbl)
@@ -128,10 +149,8 @@ public class InvestCalc extends javax.swing.JFrame {
                             .addComponent(princeTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(princeLbl))
                         .addGap(18, 18, 18)
-                        .addComponent(calcBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 257, Short.MAX_VALUE))
-                    .addComponent(jScrollPane1))
-                .addContainerGap())
+                        .addComponent(calcBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(18, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -184,8 +203,10 @@ public class InvestCalc extends javax.swing.JFrame {
             chart += "\t\t" + money.format(intrestAmtYear) + "\t\t" + money.format(prince);
         }
         
-        outputTxtar.setText(chart);
-        //System.out.println(chart);
+        
+        
+        //outputTxtar.setText(chart);
+        System.out.println(chart);
     }//GEN-LAST:event_calcBtnActionPerformed
 
     /**
@@ -227,8 +248,8 @@ public class InvestCalc extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton calcBtn;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea outputTxtar;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTable outputTbl;
     private javax.swing.JLabel princeLbl;
     private javax.swing.JTextField princeTxt;
     private javax.swing.JLabel rateLbl;
